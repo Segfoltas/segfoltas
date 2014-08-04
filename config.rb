4a21a9903ebbf2ac8909839fbe29fa2d4ae5c611
Mantas Norvaiša <mntnorv@gmail.com>
2014-08-04 21:16:10 +0300

    Minify CSS and JS on deployment

--- config.rb
+++ config.rb
@@ -51,15 +51,13 @@ set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 set :images_dir, 'images'
 
-activate :asset_hash
-
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 
   # Enable cache buster
   # activate :asset_hash
