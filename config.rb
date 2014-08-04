a126bd79dd4e7b85cd7faf32305936bd027ca0da
Mantas Norvaiša <mntnorv@gmail.com>
2014-08-04 21:14:37 +0300

    Add the main page

--- config.rb
+++ config.rb
@@ -48,10 +48,10 @@
 # end
 
 set :css_dir, 'stylesheets'
-
 set :js_dir, 'javascripts'
-
 set :images_dir, 'images'
+
+activate :asset_hash
 
 # Build-specific configuration
 configure :build do
---
+++ source/javascripts/application.js
@@ -0,0 +1,31 @@
+//
+// Libs
+//= require plugins/jquery-2.1.1.min
+//= require plugins/viewport/jquery.viewport
+//= require plugins/easing/jquery.easing.1.3
+//= require plugins/simpleplaceholder/jquery.simpleplaceholder
+//= require plugins/fitvids/jquery.fitvids
+//= require plugins/animations/animate
+//= require plugins/superfish/hoverIntent
+//= require plugins/superfish/superfish
+//= require plugins/revolutionslider/jquery.themepunch.plugins.min
+//= require plugins/revolutionslider/jquery.themepunch.revolution.min
+//= require plugins/bxslider/jquery.bxslider.min
+//= require plugins/magnificpopup/jquery.magnific-popup.min
+//= require plugins/isotope/imagesloaded.pkgd.min
+//= require plugins/isotope/isotope.pkgd.min
+//= require plugins/parallax/jquery.parallax.min
+//= require plugins/easypiechart/jquery.easypiechart.min
+//= require plugins/itplayer/jquery.mb.YTPlayer
+//= require plugins/easytabs/jquery.easytabs.min
+//= require plugins/waypoints/waypoints.min
+//= require plugins/waypoints/waypoints-sticky.min
+//= require plugins/jqueryvalidate/jquery.validate.min
+//= require plugins/jqueryform/jquery.form.min
+//= require plugins/gmap/jquery.gmap.min
+//= require plugins/twitter/twitterfetcher
+//
+// Template JS
+//= require scripts
+//= require plugins
+//
